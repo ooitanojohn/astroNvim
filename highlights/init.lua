b0cd662f65f1be2e6b2a96c7653600c9876661d6
ooitanojohn <qmkh733310@gmail.com>
2023-12-19 21:10:06 +0900

    init 色合いがおかしい

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,6 +1,6 @@
 return {
   -- this table overrides highlights in all themes
-  Normal = { bg = "" },
+  -- Normal = { bg = "" },
   -- -- this table overrides background highlights in all themes
   -- background = { bg = "" },
   -- -- this table overrides foreground highlights in all themes
